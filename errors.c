42897008864670e92b5e8e69d9b4fb1c76465a0d
ChristianVaras <christianvaras.2020@gmail.com>
2022-05-12 11:45:13 -0500

    Adding errors functions y fix betty style for main

---
+++ errors.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * @brief
+ *
+ */
+
+void inst_error(FILE *fd, char *line, stack_t *stack, char *count, int item)
+{
+	dprintf(STDERR_FILENO, "L%u: unknown instruction %s\n", item, count);
+	fclose(fd);
+	free(line);
+	free_dlistint(stack);
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * @brief
+ *
+ */
+
+void push_error(FILE *fd, char *line, stack_t *stack, int count)
+{
+	dprintf(STDERR_FILENO, "L%u: usage: push integer\n", count);
+	fclose(fd);
+	free(line);
+	free_dlistint(stack);
+	exit(EXIT_FAILURE);
+}
--- main.c
+++ main.c
@@ -1,54 +1,54 @@
 #include "monty.h"
 
-
 int main(int argc, char *argv[])
 {
-        if (argc != 2)
-        {
-                dprintf(STDERR_FILENO, "USAGE: monty file\n");
-                exit(EXIT_FAILURE);
-        }
-        handler(argv[1]);
-        return (0);
+	if (argc != 2)
+	{
+		dprintf(STDERR_FILENO, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	handler(argv[1]);
+	return (0);
 }
 
 void handler(char *filename)
 {
-        char *buffer = NULL;
-        char *line;
-        int line_count = 0, match = 0;
-        size_t buffsize = 0;
-        char *op_code = NULL, *argument = NULL;
-        stack_t *stack = NULL;
-        FILE *file = fopen(filename, "r");
+	char *buffer = NULL;
+	char *line;
+	int line_count = 0, match = 0;
+	size_t buffsize = 0;
+	char *op_code = NULL, *argument = NULL;
+	stack_t *stack = NULL;
+	FILE *file = fopen(filename, "r");
 
-        if (file == NULL)
-        {
-                dprintf(STDERR_FILENO, "Error: Can't open file %s\n", filename);
-                exit(EXIT_FAILURE);
-        }
-        while (getline(&buffer, &buffsize, file) != -1)
-        {
-                line_count++;
-                op_code = strtok(line, " \n\t\r");
-                if (op_code == NULL)
-                {
-                        free(op_code);
-                        continue;
-                }
-                else if (*op_code == '#')
-                        continue;
+	if (file == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", filename);
+		exit(EXIT_FAILURE);
+	}
+	while (getline(&buffer, &buffsize, file) != -1)
+	{
+		line_count++;
+		op_code = strtok(line, " \n\t\r");
+		if (op_code == NULL)
+		{
+			free(op_code);
+			continue;
+		}
+		else if (*op_code == '#')
+			continue;
 		argument = strtok(NULL, " \n\t\r");
-                match = get_opc(&stack, op_code, argument, line_count);
-                if (match == 1)
-                        push_error(file, line, stack, line_count);
-                else if (match == 2)
-                        instruction_error(file, line, stack, op_code, line_count);
-                }
-        free(buffer);
-        free_dlistint(stack);
-        fclose(file);
+		match = get_opc(&stack, op_code, argument, line_count);
+		if (match == 1)
+			push_error(file, line, stack, line_count);
+		else if (match == 2)
+			inst_error(file, line, stack, op_code, line_count);
+	}
+	free(buffer);
+	free_dlistint(stack);
+	fclose(file);
 }
+
 /**
  * get_opc - function to handle the opcode
  * @stack: is a stack or queue
@@ -61,30 +61,30 @@ int get_opc(stack_t **stack, char *oprt, char *item, int count)
 {
 	int i = 0;
 
-        instruction_t op[] = {
-                {"push", _push},
-                {"pall", _pall},
-                {NULL, NULL}
-        };
+	instruction_t op[] = {
+		{"push", _push},
+		{"pall", _pall},
+		{NULL, NULL}
+	};
 
-        while (op[i].opcode)
-        {
-                if (!strcmp(oprt, op[i].opcode))
-                {
-                        if (!strcmp(oprt, "push"))
-                        {
-                                if (_isdigit(item) == 1)
-                                        data_item = atoi(item);
-                                else
-                                        return (1);
-                        }
-                        op[i].f(stack, (unsigned int)count);
-                        break;
-                }
-                i++;
-        }
-        if (!op[i].opcode)
-                return (2);
+	while (op[i].opcode)
+	{
+		if (!strcmp(oprt, op[i].opcode))
+		{
+			if (!strcmp(oprt, "push"))
+			{
+				if (_isdigit(item) == 1)
+					data_item = atoi(item);
+				else
+					return (1);
+			}
+			op[i].f(stack, (unsigned int)count);
+			break;
+		}
+		i++;
+	}
+	if (!op[i].opcode)
+		return (2);
 
-        return (0);
+	return (0);
 }
