2ce3baf533d031349238b38c5dfb0e8f96166d3f
BigDany1792 <dany1792dota@gmail.com>
2022-05-12 20:04:01 -0500

    ci: added new functions and added prototypes

--- functions_monty1.c
+++ functions_monty1.c
@@ -83,12 +83,22 @@ void _pop(stack_t **stack, unsigned int line_number)
 }
 
 /**
- * _nop - function void
- * @stack: Stack list
+ * _swap - swaps the top two elements of the stack.
+ * @stack: Stack
  * @line_number: Number of the line
  */
-void _nop(stack_t **stack, unsigned int line_number)
+void _swap(stack_t **stack, unsigned int line_number)
 {
-	(void)stack;
-	(void)line_number;
+	stack_t *current = *stack;
+	int temp = 0;
+
+	if (!*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		cleanStack(stack);
+		exit(EXIT_FAILURE);
+	}
+	temp = current->n;
+	current->n = current->next->n;
+	current->next->n = temp;
 }
