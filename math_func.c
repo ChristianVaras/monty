ce96facb38cc3dfea0207531091fe8a00ae78240
BigDany1792 <dany1792dota@gmail.com>
2022-05-12 20:14:47 -0500

    ci: added function that add

--- math_func.c
+++ math_func.c
@@ -7,15 +7,21 @@
  */
 void _add(stack_t **stack, unsigned int line_number)
 {
-	if (*stack == NULL || (*stack)->next == NULL)
+	stack_t *current = NULL;
+	int sum = 0;
+
+	if (!*stack || !(*stack)->next)
 	{
-		printf("L%d: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		cleanStack(stack);
 		exit(EXIT_FAILURE);
 	}
-	(*stack)->next->n += (*stack)->n;
+	current = *stack;
+	sum = current->n + current->next->n;
+	current->next->n = sum;
 	_pop(stack, line_number);
 }
+
 
 /**
  * _sub - subtracts top of stack and second top of stack
